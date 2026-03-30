5c2cacd09954e5c45198668349fc7f5e3792aebb
Roco-LEE <aowlrtmd@gmail.com>
2026-03-30 20:02:48 +0900

    sign in

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Main from './layouts/Main'
 import ProductList from './pages/ProductList'
 import ProductInfo from './pages/ProductInfo'
 import AddProduct from './pages/AddProduct'
+import SignIn from './pages/SignIn'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
             <Route path='/products' element={<ProductList></ProductList>} />
             <Route path='/products/:id' element={<ProductInfo></ProductInfo>}/>
             <Route path='/add-product' element={<AddProduct></AddProduct>}/>
+            <Route path='/signin' element={<SignIn></SignIn>}/>
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/data/users.js
@@ -0,0 +1,5 @@
+export const users = [
+    {userId:'admin', password:'1234'},
+    {userId:'manager', password:'1234'},
+    {userId:'tester', password:'1234'}
+]
